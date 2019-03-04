f70db39bcc78c30cb53917d43a65d65a5597648e
bgauduch <bgauduch@users.noreply.github.com>
2019-03-04 12:54:41 +0100

    :wrench: corrected env script

--- TP2-skeleton/env-init.sh
+++ TP2-skeleton/env-init.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env sh
 
-set -eo pipefail
-
 # pull docker image
-bash -c "docker pull codersociety/terraform-azure-cli"
+docker pull codersociety/terraform-azure-cli
 
 # launch container with current folder mounted
-bash -c "docker container run -it -v $(pwd):/root codersociety/terraform-azure-cli"
+docker container run -it -v "$(pwd)":/root codersociety/terraform-azure-cli
