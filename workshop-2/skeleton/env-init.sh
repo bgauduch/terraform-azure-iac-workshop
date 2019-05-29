371b27542c3db5486c6c25e019a04fa62d63d9b9
bgauduch <bgauduch@users.noreply.github.com>
2019-05-29 12:06:21 +0200

    :whale: force image pull in env setup script

--- workshop-2/skeleton/env-init.sh
+++ workshop-2/skeleton/env-init.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
 
+# latest version is pulled
+docker image pull zenika/terraform-azure-cli
+
 # launch container with current folder mounted
 docker container run -it -v "$(pwd)":/workspace zenika/terraform-azure-cli:latest
--- workshop-2/solution/env-init.sh
+++ workshop-2/solution/env-init.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
 
+# enforce image latest version is pulled
+docker image pull zenika/terraform-azure-cli
+
 # launch container with current folder mounted
-docker container run -it -v "$(pwd)":/workspace zenika/terraform-azure-cli:latest
+docker container run -it -v "$(pwd)":/workspace zenika/terraform-azure-cli
